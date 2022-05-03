a42e0769e684d5edd4d83fb547912295a4c2a937
KHTangent <contact@khtangent.com>
2022-05-03 17:55:49 +0200

    Add API to get voting

---
+++ server/api/voting/[id].get.ts
@@ -0,0 +1,20 @@
+import Voting from "~~/server/models/voting";
+import useDbPool from "../../utils/useDbPool";
+import useLogin from "../../utils/useLogin";
+
+export default defineEventHandler(async (e) => {
+	const pool = await useDbPool(e);
+	const user = await useLogin(e);
+	const voteId = e.context.params.id;
+	const voting = await Voting.fromUuid(pool, voteId, user.id);
+	return {
+		id: voting.id,
+		title: voting.title,
+		options: voting.options.map((e) => {
+			return {
+				name: e.name,
+				votes: e.votes,
+			};
+		}),
+	};
+});
